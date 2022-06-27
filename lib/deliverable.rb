5979163e6f888415eb12628b1b2cc8b29fc3f5da
Clement Ferey <clement.ferey@gmail.com>
2022-06-27 12:33:36 +0200

    Require necessary file

--- lib/deliverable.rb
+++ lib/deliverable.rb
@@ -2,6 +2,7 @@ require "deliverable/version"
 require "deliverable/engine"
 require "deliverable/outgoing_email_observer"
 require "deliverable/sendgrid_event_processor"
+require "deliverable/postmark_event_processor"
 
 module Deliverable
   ActiveSupport.on_load(:action_mailer) do
